d1a902082b326b83b6dd70b8e9ef9c02339e41d6
abdurrahman <abdurrahmanbasgoynuk@gmail.com>
2024-01-07 22:13:57 +0300

    Initial commit

---
+++ src/main/java/com/codexist/locationsearchapi/LocationSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.codexist.locationsearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationSearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codexist/locationsearchapi/LocationSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codexist.locationsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
